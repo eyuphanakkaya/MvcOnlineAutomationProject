3ca5cf4bb4fadb5b9c307ebaa27b2431be7326f6
Eyüphan Akkaya <96181594+eyuphanakkaya@users.noreply.github.com>
2023-01-15 19:04:09 +0300

    Update

---
+++ Controllers/DepartmantController.cs
@@ -0,0 +1,33 @@
+﻿using MvcOnlineAutomationProject.Models.EntityFramework;
+using MvcOnlineAutomationProject.Models.EntityFramwork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineAutomationProject.Controllers
+{
+    public class DepartmantController : Controller
+    {
+        // GET: Departmant
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var Values = context.Departmants.ToList();
+            return View(Values);
+        }
+        [HttpGet]
+        public ActionResult AddDepartmant()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddDepartmant(Departmant departmant)
+        {
+            context.Departmants.Add(departmant);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
